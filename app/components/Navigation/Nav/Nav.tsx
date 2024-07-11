cabb2e4816ce04dfb682aa7a6f231cb4e3872852
DimitarSergeev <sergeev88@abv.bg>
2024-07-11 15:53:35 +0300

    stage nav

--- app/components/Navigation/Nav/Nav.tsx
+++ app/components/Navigation/Nav/Nav.tsx
@@ -5,11 +5,40 @@ import { Avatar } from "primereact/avatar";
 import { useState } from "react";
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
   const toggleIsOpen = () => {
     setIsOpen((prev) => !prev);
   };
+   const toggleSearch = (value : boolean) => {
+     setSearchOpen((prev) => value);
+   };
   return (
     <nav className={styles.navigation}>
+      <div className={styles.headerSearch}>
+        <div className={`${styles.mainSearch}`}>
+          <div
+            className={`${styles.inputGroup} ${searchOpen ? styles.open : ""}`}
+          >
+            <span
+              className="input-group-prepend search-close"
+              onClick={() => toggleSearch(false)}
+            >
+              <i className="fa-solid fa-xmark" />
+            </span>
+            <input
+              type="text"
+              className={`${styles.formControl}`}
+              placeholder="Enter Keyword"
+            />
+            <span
+              className="input-group-append search-btn"
+              onClick={() => toggleSearch(true)}
+            >
+              <i className="fa-solid fa-magnifying-glass" />
+            </span>
+          </div>
+        </div>
+      </div>
       <div className={styles.user} onClick={toggleIsOpen}>
         <div className={styles.wrapper}>
           <Avatar image="/images/logo.jpg" size="normal" shape="circle" />
@@ -17,9 +46,7 @@ export default function Nav() {
         </div>
         <i className="fa-solid fa-arrow-down"></i>
 
-        <ul
-          className={`${styles.subItems} ${isOpen ? styles.show : ""}`}
-        >
+        <ul className={`${styles.subItems} ${isOpen ? styles.show : ""}`}>
           <li>
             <i className="fa-solid fa-user"></i>
             <span>Profile</span>
