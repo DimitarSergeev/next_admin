21aebc600c0928a562cc6bde4b56e7d7e48740a8
DimitarSergeev <sergeev88@abv.bg>
2024-07-15 12:05:18 +0300

    remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import { LoginButton, LogoutButton } from "./auth";
-import { User } from "./user";
 
 export default async function Home() {
 
   return (
     <main>
-      <LoginButton />
-      <LogoutButton />
       <h2>Server Session</h2>
-      <h2>Client Call</h2>
-      <User />
     </main>
   );
 }
